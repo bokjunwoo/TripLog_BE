1672d04595b8a16032c1af203be390395676d948
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-30 23:48:15 +0900

    feat : 좋아요 기능

--- controllers/like.js
+++ controllers/like.js
@@ -3,31 +3,53 @@ const mongoClient = require('../routes/mongo');
 const _client = mongoClient.connect();
 
 const likeDB = {
-  likeIncPlus: async (contentid) => {
+  likeIncPlus: async (data) => {
     const client = await _client;
-    const db = client.db('triplog').collection('contentid');
-    const likePlus = await db.updateOne(
-      { contentid: contentid },
+    
+    const reviewData = client.db('triplog').collection(`${data.region}`);
+    const likePlus = await reviewData.updateOne(
+      { contentid: data.contentid },
       { $inc: { like: +1 } }
     );
 
-    if (likePlus.acknowledged) {
+    const likeData = client.db('triplog').collection('contentid');
+    const likeInc =  await likeData.updateOne(
+      { contentid: data.contentid },
+      { $inc: { like: +1 } }
+    )
+    const likeUserUpdata = await likeData.updateOne(
+      { contentid: data.contentid },
+      { $push: { likeuser: data.nickName } }
+    );
+
+    if (likePlus.acknowledged && likeUserUpdata.acknowledged && likeInc.acknowledged) {
       return likePlus;
     } else {
       throw new Error('통신 이상');
     }
   },
 
-  likeIncMinus: async (contentid) => {
+  likeIncMinus: async (data) => {
     const client = await _client;
-    const db = client.db('triplog').collection('contentid');
-    const likeMinus = await db.updateOne(
-      { contentid: contentid },
+    
+    const reviewData = client.db('triplog').collection(`${data.region}`);
+    const likePlus = await reviewData.updateOne(
+      { contentid: data.contentid },
       { $inc: { like: -1 } }
     );
 
-    if (likeMinus.acknowledged) {
-      return true;
+    const likeData = client.db('triplog').collection('contentid');
+    const likeInc =  await likeData.updateOne(
+      { contentid: data.contentid },
+      { $inc: { like: -1 } }
+    )
+    const likeUserUpdata = await likeData.updateOne(
+      { contentid: data.contentid },
+      { $pull: { likeuser: data.nickName } }
+    );
+
+    if (likePlus.acknowledged && likeUserUpdata.acknowledged && likeInc.acknowledged) {
+      return likePlus;
     } else {
       throw new Error('통신 이상');
     }
