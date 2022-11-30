763249caf38293b835bb9be69899be7110205f18
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-30 23:50:01 +0900

    feat : params -> body 변경

--- routes/likeRouter.js
+++ routes/likeRouter.js
@@ -7,12 +7,12 @@ const mongoDB = require('../controllers/like');
 /* like update */
 // plus
 router.post('/plus', async (req, res) => {
-  const data = await mongoDB.likeIncPlus(req.body.contentid);
+  const data = await mongoDB.likeIncPlus(req.body);
   res.send(JSON.stringify(data))
 })
 // minus
 router.post('/minus', async (req, res) => {
-  const data = await mongoDB.likeIncMinus(req.body.contentid);
+  const data = await mongoDB.likeIncMinus(req.body);
   res.send(JSON.stringify(data))
 })
 
