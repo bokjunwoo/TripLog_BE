1efa72ab9397cbaee387c5db85579e886d37fb62
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-12-01 17:33:40 +0900

    comment : 주석추가

--- app.js
+++ app.js
@@ -11,20 +11,8 @@ const PORT = 4000;
 const cors = require('cors');
 app.use(cors());
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// 데이터 저장하기 위해 전송 데이터 제한해제
-app.use(express.json({
-  limit : "50mb"
-}));
-app.use(express.urlencoded({
-  limit:"50mb",
-  extended: false
-}));
-// api
-const apiRouter = require('./routes/apiRouter');
-app.use('/api', apiRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // 이미지 저장 위치
 app.use('/uploads', express.static('uploads'));
@@ -66,7 +54,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 /*
 
-/* img */
+/* image */
 app.use('/uploads', express.static('uploads'));
 
 /* routes */
