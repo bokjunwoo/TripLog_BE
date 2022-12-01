53134334c4a9f7a687b5ef1441a522c2974fdc4a
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-12-01 13:45:16 +0900

    refactor : 필요없는 라우터 삭제

--- controllers/detail.js
+++ controllers/detail.js
@@ -29,61 +29,6 @@ const checkDB = {
     const data = await db.findOne({ contentid: contentid });
     return data;
   },
-
-  // 사용자의 좋아요 항목 가져오기
-  postLikeClick: async (likeData) => {
-    const client = await _client;
-    const db = client.db('triplog').collection('likes');
-    const data = await db.findOne({ nickName: likeData.nickName });
-    return data;
-  },
-
-  // starAvg
-  incStar: async (starAge, contentId) => {
-    // console.log('@@@@@@@@@', starAge, contentId);
-    console.log(starAge);
-    const client = await _client;
-    const db = client.db('triplog').collection('detail');
-    const result = await db.updateOne(
-      { 'data.contentid': contentId },
-      { $set: starAge }
-    );
-    if (result.acknowledged) {
-      return '별점 업데이트 성공';
-    } else {
-      throw new Error('통신 이상');
-    }
-  },
-
-  // 좋아요 +1
-  incLike: async (contentId) => {
-    const client = await _client;
-    const db = client.db('triplog').collection('detail');
-    const result = await db.updateOne(
-      { 'data.contentid': contentId },
-      { $inc: { like: +1 } }
-    );
-    if (result.acknowledged) {
-      return '좋아요 + 1 업데이트 성공';
-    } else {
-      throw new Error('통신 이상');
-    }
-  },
-  
-  // 좋아요 -1
-  deleteLike: async (contentId) => {
-    const client = await _client;
-    const db = client.db('triplog').collection('detail');
-    const result = await db.updateOne(
-      { 'data.contentid': contentId },
-      { $inc: { like: -1 } }
-    );
-    if (result.acknowledged) {
-      return '좋아요 -1';
-    } else {
-      throw new Error('통신 이상');
-    }
-  },
 };
 
 module.exports = checkDB;
