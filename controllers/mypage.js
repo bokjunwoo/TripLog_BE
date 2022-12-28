8291acde4b0422583cb24c9d2f71c7d19a1ada95
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-12-28 17:42:54 +0900

    feat : mypage 이미지 라우터 추가

--- controllers/mypage.js
+++ controllers/mypage.js
@@ -3,6 +3,14 @@ const mongoClient = require('../routes/mongo');
 const _client = mongoClient.connect();
 
 const mypageDB = {
+  // 유저이미지(POST)
+  postUserImage: async (nickName) => {
+    const client = await _client;
+    const db = client.db('triplog').collection('users');
+    const data = await db.findOne({ nickName: nickName });
+    return data;
+  },
+
   // params의 데이터 가져오기(GET)
   getMypage: async (params) => {
     const client = await _client;
--- routes/mypageRouter.js
+++ routes/mypageRouter.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 const mongoDB = require('../controllers/mypage');
 
+// 유저이미지(POST)
+router.post('/userimage', async (req, res) => {
+  const data = await mongoDB.postUserImage(req.body.nickName);
+  res.send(JSON.stringify(data));
+});
+
 // params의 데이터 가져오기(GET)
 router.get('/:nickName/:option', async (req, res) => {
   const data = await mongoDB.getMypage(req.params);
