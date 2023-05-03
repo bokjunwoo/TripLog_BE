4f816dfdd45066c327a3a07cbef54e0f47b05e60
JUNU <bokjunwoo@gmail.com>
2023-05-03 23:55:33 +0900

    feat : mongoClient.connect

--- passport/index.js
+++ passport/index.js
@@ -1,22 +1,23 @@
 const passport = require('passport');
 const local = require('./local');
 const mongoClient = require('../routes/mongo');
-const _client = mongoClient.connect();
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
+    console.log('serializeUser 실행');
     done(null, user.email);
   });
 
   passport.deserializeUser(async (email, done) => {
+    console.log('deserializeUser 실행');
     try {
-      const client = await _client;
+      const client = await mongoClient.connect();
       const userdb = client.db('TripLogV2').collection('user');
       const user = await userdb.findOne({ email });
       done(null, user); // req.user
     } catch (error) {
       console.error(error);
-      done(error);
+      done(error, null);
     }
   });
 
