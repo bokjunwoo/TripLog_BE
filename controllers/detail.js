d41eb713464827d5620bbf1e890fb0740e2550cd
JUNU <bokjunwoo@gmail.com>
2023-06-02 01:40:33 +0900

    feat : params의 값이 없을 경우에 대한 구문 추가

--- controllers/detail.js
+++ controllers/detail.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const mongoClient = require('../routes/mongo');
 
 const _client = mongoClient.connect();
@@ -5,29 +6,33 @@ const _client = mongoClient.connect();
 const checkDB = {
   // 디테일 데이터(GET)
   getAlldetail: async (params) => {
-    const client = await _client;
-    const db = client.db('TripLogV2').collection(`${params.region}`);
-    const data = await db.findOne({ contentid: `${params.contentid}` });
+    if (params.region !== 'undefined' && params.id !== 'undefined') {
+      const client = await _client;
+      const db = client.db('TripLogV2').collection(`${params.region}`);
+      const data = await db.findOne({ contentid: `${params.id}` });
 
-    // 조회수 +1
-    if (data) {
-      await db.updateOne(
-        { contentid: `${params.contentid}` },
-        { $inc: { view: +1 } }
-      );
-    } else {
-      throw new Error('에러');
+      // 조회수 +1
+      if (data) {
+        await db.updateOne(
+          { contentid: `${params.id}` },
+          { $inc: { view: +1 } }
+        );
+      } else {
+        throw new Error('에러');
+      }
+
+      return data;
     }
-
-    return data;
   },
 
   // 해당 디테일 정보의 리뷰, 별점 데이터(GET)
   getEtcdetail: async (contentid) => {
-    const client = await _client;
-    const db = client.db('TripLogV2').collection('like');
-    const data = await db.findOne({ contentid: contentid });
-    return data;
+    if (contentid !== 'undefined') {
+      const client = await _client;
+      const db = client.db('TripLogV2').collection('like');
+      const data = await db.findOne({ contentid: contentid });
+      return data;
+    }
   },
 };
 
--- routes/detailRouter.js
+++ routes/detailRouter.js
@@ -5,14 +5,15 @@ const router = express.Router();
 const mongoDB = require('../controllers/detail');
 
 // 디테일 데이터(GET)
-router.get('/:region/:contentid', async (req, res) => {
+router.get('/:region/:id', async (req, res) => {
+  console.log(req.params)
   const data = await mongoDB.getAlldetail(req.params);
   res.send(JSON.stringify(data));
 });
 
 // 해당 디테일 정보의 리뷰, 별점 데이터(GET)
-router.get('/:contentid', async (req, res) => {
-  const data = await mongoDB.getEtcdetail(req.params.contentid);
+router.get('/:id', async (req, res) => {
+  const data = await mongoDB.getEtcdetail(req.params.id);
   res.send(JSON.stringify(data));
 });
 
