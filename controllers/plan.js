6e8a2b7d8059e9077a19966d54ed32f1771f727f
JUNU <bokjunwoo@gmail.com>
2023-07-14 23:50:48 +0900

    feat : searchPlan 구현

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const mongoClient = require('../routes/mongo');
 
 // _여러번 실행되는 걸 막음
@@ -5,14 +6,26 @@ const _client = mongoClient.connect();
 
 const planDB = {
   // 여행 저장(POST)
-  savePlan: async (list) => {
+  addPlan: async (data) => {
     const client = await _client;
     const db = client.db('TripLogV2').collection('plan');
-    const plan = await db.insertOne(list);
+    const plan = await db.insertOne(data);
     if (plan.acknowledged) {
       return true;
     } else {
       throw new Error('통신이상');
+    }
+  },
+
+  searchPlan: async (search) => {
+    const client = await _client;
+    const db = client.db('TripLogV2').collection(`${search.region}`);
+    const data = await db.findOne({ title: `${search.search}` });
+
+    if (data) {
+      return data;
+    } else {
+      return null;
     }
   },
 };
--- routes/planRouter.js
+++ routes/planRouter.js
@@ -4,9 +4,16 @@ const router = express.Router();
 
 const mongoDB = require('../controllers/plan');
 
+const { isLoggedIn } = require('../passport/middleware');
+
 // 내 여행 저장
-router.post('/', async (req, res) => {
-  const data = await mongoDB.savePlan(req.body);
+router.post('/add', isLoggedIn, async (req, res) => {
+  const data = await mongoDB.addPlan(req.body);
+  res.send(JSON.stringify(data));
+});
+
+router.post('/search', isLoggedIn, async (req, res) => {
+  const data = await mongoDB.searchPlan(req.body);
   res.send(JSON.stringify(data));
 });
 
