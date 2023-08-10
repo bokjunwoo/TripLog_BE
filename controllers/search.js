593018fbc449a20b4743cc74c92d1e256fb31f82
JUNU <bokjunwoo@gmail.com>
2023-08-10 23:59:16 +0900

    feat : searchDB getList 구현

---
+++ controllers/search.js
@@ -0,0 +1,24 @@
+const mongoClient = require('../routes/mongo');
+
+const _client = mongoClient.connect();
+
+const searchDB = {
+  // params의 데이터 가져오기(GET)
+  getList: async (query) => {
+    const client = await _client;
+    const db = client.db('TripLogV2').collection(`${query.region}`);
+    const page = query.page[0] || 1; // 기본 페이지는 1
+    const limit = 12; // 페이지당 리스트 개수
+    const skipIndex = (page - 1) * limit;
+    const data = await db
+      .find({ type: `${query.type}` })
+      .skip(skipIndex)
+      .limit(limit)
+      .toArray();
+    const total = await db.countDocuments({ type: query.type });
+
+    return { data, total };
+  },
+};
+
+module.exports = searchDB;
