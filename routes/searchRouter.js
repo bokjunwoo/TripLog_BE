ada4d74f6b5910661382fad101c10f14d082d18e
JUNU <bokjunwoo@gmail.com>
2023-08-14 21:04:01 +0900

    fix : search controllers 수정

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const mongoDB = require('../controllers/list');
+const mongoDB = require('../controllers/search');
 
 // params의 데이터 가져오기(GET)
-router.get('/:title', async (req, res) => {
+router.get('/', async (req, res) => {
   const data = await mongoDB.getList(req.query);
   res.send(JSON.stringify(data));
 });
