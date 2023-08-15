e2eb7a0a83154650f66a8825b54d28c60727f2fd
JUNU <bokjunwoo@gmail.com>
2023-08-15 23:48:43 +0900

    feat : searchDB retrun 값 추가

--- controllers/search.js
+++ controllers/search.js
@@ -5,19 +5,43 @@ const _client = mongoClient.connect();
 const searchDB = {
   // params의 데이터 가져오기(GET)
   getList: async (query) => {
+    console.log(query);
     const client = await _client;
     const db = client.db('TripLogV2').collection(`${query.region}`);
-    const page = query.page[0] || 1; // 기본 페이지는 1
-    const limit = 12; // 페이지당 리스트 개수
-    const skipIndex = (page - 1) * limit;
-    const data = await db
-      .find({ type: `${query.type}` })
-      .skip(skipIndex)
-      .limit(limit)
-      .toArray();
-    const total = await db.countDocuments({ type: query.type });
 
-    return { data, total };
+    const page = parseInt(query.page) || 1; // 기본 페이지는 1
+    const limit = 12; // 페이지당 리스트 개수
+
+    // 데이터 검색 쿼리
+    const searchQuery = {
+      title: {
+        $regex: query.title,
+        $options: 'i',
+      },
+    };
+
+    // 전체 아이템 개수 가져오기
+    const totalItems = await db.countDocuments(searchQuery);
+
+    // 전체 페이지 수 계산
+    const totalPages = Math.ceil(totalItems / limit);
+
+    // 스킵할 아이템 수를 계산하여 페이지 설정
+    const skipItems = (page - 1) * limit;
+
+    const searchRequirements = [
+      { $match: searchQuery }, // $match 연산자를 사용하여 검색
+      { $skip: skipItems }, // 스킵할 아이템 수 설정
+      { $limit: limit }, // 페이지당 아이템 수 설정
+    ];
+
+    const data = await db.aggregate(searchRequirements).toArray();
+
+    return {
+      data,
+      currentPage: page,
+      totalPages,
+    };
   },
 };
 
